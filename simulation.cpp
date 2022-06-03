fec531a5d7d9c11e544ade12e58fffdf1672100b
Meithan West <meithan@gmail.com>
2022-06-03 14:16:19 -0500

    Tweak default params

--- simulation.cpp
+++ simulation.cpp
@@ -14,11 +14,11 @@ int main() {
   // SIMULATION PARAMETERS
 
   const int num_agents = 1e6;
-  const int num_initially_infected = 100;
+  const int num_initially_infected = 1000;
   const int base_encounters = 10;
 
   const int num_iterations = 0;
-  bool verbose = false;
+  bool verbose = true;
 
   char states_fname[] = "states.txt";
 
@@ -28,13 +28,13 @@ int main() {
   // DISEASE
 
   string name = "COVID19 variant 1";
-  // double transm_prob = 0.05;
   double transm_prob = 0.10;
+  // double transm_prob = 0.10;
   double latency_period = 2;
   double incubation_period = 3;
   double symptoms_duration = 5;
   double contagious_duration = (incubation_period + symptoms_duration) - latency_period;
-  double max_severity = 1;
+  double max_severity = 2;
   double fatality_rate = 0.01;
   Disease* variant1 = new Disease(name, transm_prob, latency_period, contagious_duration, incubation_period, symptoms_duration, max_severity, fatality_rate);
 
@@ -48,7 +48,8 @@ int main() {
 
   ep->run_simulation(num_iterations, verbose);
   
-  int tot_infected = ep->num_agents - ep->states_counts[STATE_HEALTHY];
-  printf("Population infected: %.1f%%\n", 100*(double)tot_infected/num_agents);
+  int tot_infected = num_agents - ep->states_counts[STATE_HEALTHY];
+  double pct_infected = 100*(double)tot_infected/num_agents;
+  printf("Population infected: %i : %.1f%%\n", tot_infected, pct_infected);
 
 }
